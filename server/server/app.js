4c1b95dcebd3ed41cf3e3bf9c6e59516075ba899
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2021-07-23 18:44:58 +0500

    add mongodb

--- server/server/app.js
+++ server/server/app.js
@@ -1,11 +1,25 @@
 const express = require("express")
 const graphqlHTTP = require("express-graphql")
+const mongoose = require("mongoose")
 const schema = require("../schema/schema")
 
 const app = express()
 const PORT = 3005
 
+mongoose.connect(
+  "mongodb+srv://igorg99:srmyNMI96GnHbtn5@cluster0.yf2hy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  }
+)
+
 app.use("/graphql", graphqlHTTP({ schema, graphiql: true }))
+
+const dbConnection = mongoose.connection
+dbConnection.on("error", (err) => console.log(`Connection error: ${err}`))
+dbConnection.once("open", () => console.log("Connected to DB!"))
 
 app.listen(PORT, (err) => {
   err ? console.log(err) : console.log("Server started!")
