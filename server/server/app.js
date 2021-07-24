d24963828e921716c11c5948f699de3dc392cfce
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2021-07-24 11:18:58 +0500

    connection back and front

--- server/server/app.js
+++ server/server/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const graphqlHTTP = require("express-graphql")
 const mongoose = require("mongoose")
+const cors = require('cors')
 const schema = require("../schema/schema")
 
 const app = express()
@@ -14,6 +15,8 @@ mongoose.connect(
     useFindAndModify: false,
   }
 )
+
+app.use(cors())
 
 app.use("/graphql", graphqlHTTP({ schema, graphiql: true }))
 
