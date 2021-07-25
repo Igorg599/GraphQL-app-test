d9492cf67603b0fa2bdf4b7e71b211293c044560
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2021-07-25 17:32:02 +0500

    search elements in DB

--- application/src/components/MoviesTable/queries.js
+++ application/src/components/MoviesTable/queries.js
@@ -1,16 +1,16 @@
 import { gql } from 'apollo-boost';
 
 export const moviesQuery = gql`
-  query moviesQuery {
-    movies {
+  query moviesQuery($name: String) {
+    movies(name: $name) {
       id
       name
       genre
       watched
       rate
       director {
-        id
         name
+        id
       }
     }
   }
